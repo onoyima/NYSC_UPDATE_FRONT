58f2fc6fa8f5a94ae40e9330bcd0e3c23ebaff13
onoyima <clintonfaze@outlook.com>
2026-07-19 17:02:48 +0100

    Move NIN to Academic Information section

--- app/student/confirm/page.tsx
+++ app/student/confirm/page.tsx
@@ -362,7 +362,15 @@ const DataConfirmationPage: React.FC = () => {
                         </Select>
                       </div>
                     </div>
+                  </CardContent>
+                </Card>
 
+                {/* Academic Information */}
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Academic Information</CardTitle>
+                  </CardHeader>
+                  <CardContent className="space-y-4">
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                       <div>
                         <Label htmlFor="nin">NIN (11 digits) *</Label>
@@ -375,17 +383,6 @@ const DataConfirmationPage: React.FC = () => {
                           required
                         />
                       </div>
-                    </div>
-                  </CardContent>
-                </Card>
-
-                {/* Academic Information */}
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Academic Information</CardTitle>
-                  </CardHeader>
-                  <CardContent className="space-y-4">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                       <div>
                         <Label htmlFor="matric_no">Matriculation Number *</Label>
                         <Input
